6e41630faec7cd0b5c0dc613841c44bc881b8a2a
NoizuHika <47974646+NoizuHika@users.noreply.github.com>
2023-01-30 17:03:13 +0100

    Update main.cpp

--- NAIKRESKI8/main.cpp
+++ NAIKRESKI8/main.cpp
@@ -1,70 +1,72 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
+#include <iostream>
 
-using namespace cv;
-using namespace std;
+int main(int argc, char **argv) {
+    using namespace std;
+    using namespace cv;
 
-int main() {
-    VideoCapture cap(0);
+    //Tworzenie dwóch tablic lower i upper o rozmiarze 3, które określają dolną i górną granicę dla koloru.
+    array<int, 3> lower = {135, 90, 0};
+    array<int, 3> upper = {255, 255, 255};
 
-    Mat frame, hsv_frame, mask;
+    VideoCapture vc(0); //otwarcie kamery
+    cv::Mat frame, dst, pierwszy;
 
-    while (true) {
-        cap >> frame;
+    while (waitKey(10) != 27) {
+        vc >> frame;
+        //Przekształcenie koloru z BGR na HSV za pomocą funkcji cvtColor.
+        cvtColor(frame, dst, COLOR_BGR2HSV);
+        //Odfiltrowanie koloru za pomocą funkcji inRange.
+        inRange(dst, lower, upper, pierwszy);
 
-        cvtColor(frame, hsv_frame, COLOR_BGR2HSV);
+        imshow("pierwszy", pierwszy);
+        imshow("frame", frame);
 
-        inRange(hsv_frame, Scalar(0, 100, 100), Scalar(10, 255, 255), mask);
+        vector<vector<Size>> contours;
+        //Znalezienie konturów za pomocą funkcji findContours.
+        findContours(pierwszy, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 
-        vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
+        //jezeli 2 kontury to sortuj ich
+        if (contours.size() >= 2) {
 
-        findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+            //pobierz dwa największe.
+            sort(contours.begin(), contours.end(), [](vector<Size> a, vector<Size> b) {
+                return contourArea(a) > contourArea(b);
+            });
 
-        double max_area1 = 0;
-        double max_area2 = 0;
-        int max_index1 = 0;
-        int max_index2 = 0;
+            //Oblicz momenty każdego z konturów.
+            auto big_con = moments(contours[0], false);
+            auto big_con_sec = moments(contours[1], false);
 
-        for (int i = 0; i < contours.size(); i++) {
-            double area = contourArea(contours[i]);
-            if (area > max_area1) {
-                max_area2 = max_area1;
-                max_area1 = area;
-                max_index2 = max_index1;
-                max_index1 = i;
-            } else if (area > max_area2) {
-                max_area2 = area;
-                max_index2 = i;
-            }
-        }
+            //Oblicz środek ciężkości dla każdego konturu i przypisz do zmiennych
+            int x1 = (int) (big_con.m10 / big_con.m00);
+            int y1 = (int) (big_con.m01 / big_con.m00);
 
-        int margin = 10;
-        int x1 = min(contours[max_index1][0].x, contours[max_index1][contours[max_index1].size() - 1].x);
-        int x2 = min(contours[max_index2][0].x, contours[max_index2][contours[max_index2].size() - 1].x);
-        int y1 = min(contours[max_index1][0].y, contours[max_index1][contours[max_index1].size() - 1].y);
-        int y2 = min(contours[max_index2][0].y, contours[max_index2][contours[max_index2].size() - 1].y);
-        if (abs(y1 - y2) < margin && x1 < x2) {
+            int x2 = (int) (big_con_sec.m10 / big_con_sec.m00);
+            int y2 = (int) (big_con_sec.m01 / big_con_sec.m00);
 
-            drawContours(frame, contours, max_index1, Scalar(0, 0, 255), 2);
-            drawContours(frame, contours, max_index2, Scalar(0, 0, 255), 2);
+            imshow("frame", frame);
 
             double distance = x2 - x1;
 
-            double max_width = 20;
+            double max_width = 5;
             double min_width = 2;
-
             double width = min_width + (max_width - min_width) * (1 / (1 + exp(-0.1 * (distance - 150))));
 
-            line(frame, Point(x1, y1), Point(x2, y1), Scalar(0, 255, 0), width);
-            line(frame, Point(x1, y1 + margin), Point(x2, y1 + margin), Scalar(0, 255, 0), width);
-            line(frame, Point(x1, y1 + (2 * margin)), Point(x2, y1 + (2 * margin)), Scalar(0, 255, 0), width);
+            //Jeśli różnica między środkami ciężkości jest mniejsza niż 4, rysuj trzy linie pomiędzy tymi punktami na oryginalnym obrazie.
+            if ((y1 - y2 > -4) && (y1 - y2 < 4) ) {
+                line(frame, Point (x1, y1 + 10),
+                     Point (x2, y2 + 10), {0,255,0}, width);
+                line(frame, Point (x1, y1 - 10),
+                     Point (x2, y2 - 10), {0,255,0}, width);
+                line(frame, Point (x1, y1 - 0),
+                     Point (x2, y2 - 0), {0,255,0}, width);
+
+                imshow("frame", frame);
+            }
         }
-
-        imshow("frame", frame);
-
-        if (waitKey(30) >= 0) break;
     }
-
     return 0;
 }
