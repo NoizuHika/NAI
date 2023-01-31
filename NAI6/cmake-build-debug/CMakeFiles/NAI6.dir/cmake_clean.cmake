25546140b0db78f6900d68575d4fc9442f346db8
NoizuHika <kozyrniynero@gmail.com>
2023-01-31 22:58:31 +0100

    2p

---
+++ NAI6/cmake-build-debug/CMakeFiles/NAI6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NAI6.dir/main.cpp.obj"
+  "CMakeFiles/NAI6.dir/main.cpp.obj.d"
+  "NAI6.exe"
+  "NAI6.exe.manifest"
+  "NAI6.pdb"
+  "libNAI6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NAI6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
